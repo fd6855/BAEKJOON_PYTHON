98da13e0862ace44a534ff9e8b4331cb753d8b19
fd6855 <52096014+fd6855@users.noreply.github.com>
2023-10-23 14:36:08 +0000

    1023
    1023

---
+++ 2720.py
@@ -0,0 +1,20 @@
+n = int(input())
+a = [[0] * 4 for _ in range(n)]
+for i in range(n):
+  a[i] = int(input())
+
+for i in range(n):
+  qu, da, ni, pe = 0, 0, 0, 0
+  while a[i] >= 25:
+    qu += 1
+    a[i] -= 25
+  while a[i] >= 10:
+    da += 1
+    a[i] -= 10
+  while a[i] >= 5:
+    ni += 1
+    a[i] -= 5
+  while a[i] >= 1:
+    pe += 1
+    a[i] -= 1
+  print(qu, da, ni, pe)
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-num_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+n = int(input())
+a = [[0]*4 for _ in range(n)]
+for i in range(n):
+  a[i]=int(input())
 
-n, b = map(int,input().split())
-s = ''
-while n:
-    s += str(num_list[n%b])
-    n//= b
-print(s[::-1])
+for i in range(n):
+  qu,da,ni,pe=0,0,0,0
+  while a[i]>=25:
+    qu+=1
+    a[i]-=25
+  while a[i]>=10:
+    da+=1
+    a[i]-=10
+  while a[i]>=5:
+    ni+=1
+    a[i]-=5
+  while a[i]>=1:
+    pe+=1
+    a[i]-=1
+  print(qu,da,ni,pe)
