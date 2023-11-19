1667b24bf41d276ffdba1fe67b10017a85c3f91f
fd6855 <52096014+fd6855@users.noreply.github.com>
2023-11-19 14:02:16 +0000

    1119
    1119

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
-def count_change(a, b,N,M, chess):
-  cnt = 0
-  for i in range(a, a+8):
-    for j in range(b, b+8):
-      if i==M or j==N:
-        break
-      if chess[i][j] == "W":
-        if chess[i][j + 1] == "W":
-          cnt += 1
-          chess[i][j + 1] = "B"
-        if chess[i + 1][j] == "W":
-          cnt += 1
-          chess[i + 1][j] = "B"
-      if chess[i][j] == "B":
-        if chess[i][j + 1] == "B":
-          cnt += 1
-          chess[i][j + 1] = "W"
-        if chess[i + 1][j] == "B":
-          cnt += 1
-          chess[i + 1][j] = "W"
-  return cnt
+a,b,c,d,e,f=map(int,input().split())
 
+if c*e>f*b:
+  x=(c*e-f*b)/(a*e-d*b)
+else:
+  x=(f*b-c*e)/(d*b-a*e)
 
-N, M = map(int, input().split())
-chess = [["" for j in range(M)] for i in range(N)]
-for i in range(N):
-  a = input()
-  for j in range(M):
-    chess[i][j] = a[j]
-min_res = 2500
-res = 2500
-for a in range(0, N-8):
-  for b in range(0, M - 8):
-    res = count_change(a, b,N,M,chess)
-  min_res = min(res, min_res)
+if c*d>f*a:
+  y=(c*d-f*a)/(b*d-e*a)
+else:
+  y=(f*a-c*d)/(e*a-b*d)
 
-print(min_res)
+print(int(x),int(y))
